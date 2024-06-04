72bf7dd171bf89417dd2b71c9470eb50d8099d16
QuesoParmesano58 <miguelmsilva420@gmail.com>
2024-06-04 04:27:09 -0600

    Initial commit

---
+++ app/src/main/java/com/example/codedeluxe/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.codedeluxe
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.example.codedeluxe.databinding.ActivityMainBinding
+import java.util.Scanner
+
+
+private const val CAMERA_REQUEST_CODE = 101
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupPermissions()
+        codeScanner()
+    }
+
+    private fun codeScanner(){
+        codeScanner = CodeScanner(this, binding.scannerView)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread{
+                    binding.tvTextView.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread{
+                    Log.e("MAIN","Camera initilization error: ${it.message}")
+                }
+            }
+
+        }
+
+        binding.scannerView.setOnClickListener{
+            codeScanner.startPreview()
+        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions(){
+        val permission = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.CAMERA)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CAMERA_REQUEST_CODE ->{
+                if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "You need the camera permission to be able to use this app", Toast.LENGTH_SHORT).show()
+                }else{
+                    //successful
+                }
+            }
+        }
+    }
+
+}
